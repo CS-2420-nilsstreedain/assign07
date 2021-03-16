cc251b7ca793d22196a12c960bfb7a2d43aa09a5
PaulNuffer <78831963+PaulNuffer@users.noreply.github.com>
2021-03-15 22:45:43 -0600

    Update GraphUtility.java

--- GraphUtility.java
+++ GraphUtility.java
@@ -15,48 +15,60 @@ import java.util.Scanner;
  */
 public class GraphUtility {
 
+	/**
+	 * This method must use the depth-first search algorithm presented in lecture to
+	 * determine whether there is a path from the vertex with srcData to the vertex
+	 * with dstData in the graph. Throws an IllegalArgumentException if there does
+	 * not exist a vertex in the graph with srcData, and likewise for dstData.
+	 * 
+	 * @param <Type> The generic type for the ID of each vertex in the graph
+	 * @param sources - a list of sources
+	 * @param destinations - a list of destinations
+	 * @param srcData - the beginning vertex to start at
+	 * @param dstData - the vertex to reach
+	 * @return true if the two vertexes are connected, false otherwise
+	 * @throws IllegalArgumentException if there does not exist a vertex in the graph with srcData or dstData
+	 */
 	public static <Type> boolean areConnected(List<Type> sources, List<Type> destinations, Type srcData, Type dstData)
 			throws IllegalArgumentException {
 		// FILL IN + ADD METHOD COMMENT
 		return false;
 	}
 
-	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData, Type dstData)
-			throws IllegalArgumentException {
+	public static <Type> List<Type> shortestPath(List<Type> sources, List<Type> destinations, Type srcData,
+			Type dstData) throws IllegalArgumentException {
 		// FILL IN + ADD METHOD COMMENT
 		return null;
 	}
-	
+
 	public static <Type> List<Type> sort(List<Type> sources, List<Type> destinations) throws IllegalArgumentException {
 		// FILL IN + ADD METHOD COMMENT
 		return null;
 	}
 
 	/**
-	 * Builds "sources" and "destinations" lists according to the edges
-	 * specified in the given DOT file (e.g., "a -> b"). Assumes that the vertex
-	 * data type is String.
+	 * Builds "sources" and "destinations" lists according to the edges specified in
+	 * the given DOT file (e.g., "a -> b"). Assumes that the vertex data type is
+	 * String.
 	 * 
 	 * Accepts many valid "digraph" DOT files (see examples posted on Canvas).
-	 * --accepts \\-style comments 
-	 * --accepts one edge per line or edges terminated with ; 
-	 * --does not accept attributes in [] (e.g., [label = "a label"])
+	 * --accepts \\-style comments --accepts one edge per line or edges terminated
+	 * with ; --does not accept attributes in [] (e.g., [label = "a label"])
 	 * 
-	 * @param filename - name of the DOT file
-	 * @param sources - empty ArrayList, when method returns it is a valid
-	 *        "sources" list that can be passed to the public methods in this
-	 *        class
+	 * @param filename     - name of the DOT file
+	 * @param sources      - empty ArrayList, when method returns it is a valid
+	 *                     "sources" list that can be passed to the public methods
+	 *                     in this class
 	 * @param destinations - empty ArrayList, when method returns it is a valid
-	 *        "destinations" list that can be passed to the public methods in
-	 *        this class
+	 *                     "destinations" list that can be passed to the public
+	 *                     methods in this class
 	 */
 	public static void buildListsFromDot(String filename, ArrayList<String> sources, ArrayList<String> destinations) {
 
 		Scanner scan = null;
 		try {
 			scan = new Scanner(new File(filename));
-		}
-		catch(FileNotFoundException e) {
+		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 			System.exit(0);
 		}
@@ -65,19 +77,19 @@ public class GraphUtility {
 
 		// Determine if graph is directed (i.e., look for "digraph id {").
 		String line = "", edgeOp = "";
-		while(scan.hasNext()) {
+		while (scan.hasNext()) {
 			line = scan.next();
 
 			// Skip //-style comments.
 			line = line.replaceFirst("//.*", "");
 
-			if(line.indexOf("digraph") >= 0) {
+			if (line.indexOf("digraph") >= 0) {
 				edgeOp = "->";
 				line = line.replaceFirst(".*\\{", "");
 				break;
 			}
 		}
-		if(edgeOp.equals("")) {
+		if (edgeOp.equals("")) {
 			System.out.println("DOT graph must be directed (i.e., digraph).");
 			scan.close();
 			System.exit(0);
@@ -86,19 +98,19 @@ public class GraphUtility {
 
 		// Look for edge operator -> and determine the source and destination
 		// vertices for each edge.
-		while(scan.hasNext()) {
+		while (scan.hasNext()) {
 			String[] substring = line.split(edgeOp);
 
-			for(int i = 0; i < substring.length - 1; i += 2) {
+			for (int i = 0; i < substring.length - 1; i += 2) {
 				// remove " and trim whitespace from node string on the left
 				String vertex1 = substring[0].replace("\"", "").trim();
 				// if string is empty, try again
-				if(vertex1.equals(""))
+				if (vertex1.equals(""))
 					continue;
 
 				// do the same for the node string on the right
 				String vertex2 = substring[1].replace("\"", "").trim();
-				if(vertex2.equals(""))
+				if (vertex2.equals(""))
 					continue;
 
 				// indicate edge between vertex1 and vertex2
@@ -107,7 +119,7 @@ public class GraphUtility {
 			}
 
 			// do until the "}" has been read
-			if(substring[substring.length - 1].indexOf("}") >= 0)
+			if (substring[substring.length - 1].indexOf("}") >= 0)
 				break;
 
 			line = scan.next();
