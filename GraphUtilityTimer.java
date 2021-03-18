d2bf7032372dcefaf7e7c6f83ea25fd1ceda61eb
Nils Streedain <business@nilsstreedain.com>
2021-03-17 21:26:13 -0600

    Started Timing

---
+++ GraphUtilityTimer.java
@@ -0,0 +1,95 @@
+package assign07;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * This class collects running times for methods of GraphUtility.
+ * 
+ * @author Erin Parker, Paul Nuffer, Nils Streedain
+ * @version March 10, 2021
+ */
+public class GraphUtilityTimer {
+
+	public static void main(String[] args) {
+		System.out.println("\nN\t\tnanoTime");
+
+		int incr = 100;
+		for (int probSize = 100; probSize <= 1500; probSize += incr) {
+
+			int timesToLoop = 10000;
+			
+			 generateRandomDotFile("src/assign07/dots.txt", probSize, true);
+			 ArrayList<String> sources = new ArrayList<>();
+			 ArrayList<String> destinations = new ArrayList<>();
+			 GraphUtility.buildListsFromDot("src/assign07/dots.txt", sources, destinations);
+
+			// First, spin computing stuff until one second has gone by.
+			// This allows this thread to stabilize.
+			long stopTime, midpointTime, startTime = System.nanoTime();
+			while (System.nanoTime() - startTime < 1000000000) {
+			}
+
+			startTime = System.nanoTime();
+			
+			for (int i = 0; i < timesToLoop; i++) {
+				 GraphUtility.areConnected(sources, destinations, "v1", "v2");
+			}
+
+			midpointTime = System.nanoTime();
+
+			// Capture the cost of running the loop and any other operations done
+			// above that are not the essential method call being timed.
+			for (int i = 0; i < timesToLoop; i++) {}
+
+			stopTime = System.nanoTime();
+
+			// Compute the time, subtract the cost of running the loop
+			// from the cost of running the loop and searching.
+			// Average it over the number of runs.
+			double averageTime = ((midpointTime - startTime) - (stopTime - midpointTime)) / (double) timesToLoop;
+
+			System.out.println(probSize + "\t" + String.format("%.5f", averageTime));
+		}
+	}
+
+	/**
+	 * Private helper method to generate random dot files for timing
+	 * 
+	 * @param filename    - name of generated file
+	 * @param vertexCount - number of vertices in graph
+	 */
+	private static void generateRandomDotFile(String filename, int vertexCount, boolean cyclic) {
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(filename);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+
+		Random rng = new Random(1235967);
+
+		// randomly construct either a graph or a digraph
+		out.print("di");
+		out.println("graph G {");
+
+		// generate a list of vertices
+		String[] vertex = new String[vertexCount];
+		for (int i = 0; i < vertexCount; i++)
+			vertex[i] = "v" + i;
+
+		if (cyclic) {
+			// randomly connect the vertices using 2 * |V| edges
+			for (int i = 0; i < 2 * vertexCount; i++)
+				out.println("\t" + vertex[rng.nextInt(vertexCount)] + "->" + vertex[rng.nextInt(vertexCount)]);
+		} else {
+			for (int i = 0; i < vertexCount - 1; i++)
+				out.println("\t" + vertex[i] + "->" + vertex[i + 1 + rng.nextInt(vertexCount - (i + 1))]);
+		}
+
+		out.println("}");
+		out.close();
+	}
+}
